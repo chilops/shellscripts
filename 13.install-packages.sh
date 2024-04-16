b86627624efcf77d496409a13fc3e3e9f87cf017
Chowdarychilukuri <chilukuridevops@gmail.com>
2024-04-16 10:45:48 +0530

    added new modification

--- 13.install-packages.sh
+++ 13.install-packages.sh
@@ -5,12 +5,12 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-if [ $ID -ne 0]
+if [ $ID -ne 0 ] 
 then 
 	echo -e "$R ERROR:: Please run this script with root access $N"
 	exit 1 # you can give other than 0
 else
-	echo "$G You are root user $N"
+	echo -e "$G You are root user $N"
 fi # fi means reverse of if, indicating condition end
 
 echo "All arguments passed: $@"
